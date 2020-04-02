4a1639881f73432d9e8b428a100a780ec1e4d3d3
Josh Natis <joshnatis0@gmail.com>
2020-04-02 05:14:31 -0400

    Add <ol> functionality and refactor code into smaller functions

--- phd.cpp
+++ phd.cpp
@@ -6,10 +6,16 @@ using namespace std;
 
 #define NOT_FOUND -1
 
+
+/* _____ ____  _   _ ______ _____ _____ 
+  / ____/ __ \| \ | |  ____|_   _/ ____|
+ | |   | |  | |  \| | |__    | || |  __ 
+ | |   | |  | | . ` |  __|   | || | |_ |
+ | |___| |__| | |\  | |     _| || |__| |
+  \_____\____/|_| \_|_|    |_____\_____|*/
+
 string config(const string &inputfn)
 {
-	//============ CONFIG ============
-
 	//==============================================================
 	//* title of your HTML document/blog post
 	//* (default: name of .md file)
@@ -33,7 +39,7 @@ string config(const string &inputfn)
 	const string SYNTAX_HIGHLIGHT_STYLE = "pojoaque.css";
 	//==============================================================
 
-	//============ \CONFIG ============
+	// DON'T EDIT PAST THIS LINE :)
 
 	string header = "<!DOCTYPE html>\n";
 	header += "<html lang=\"en\">\n";
@@ -56,30 +62,53 @@ string config(const string &inputfn)
 	return header;
 }
 
-//parsing functions
+
+/*_____  _____   ____ _______ ____ _________     _______  ______  _____ 
+ |  __ \|  __ \ / __ \__   __/ __ \__   __\ \   / /  __ \|  ____|/ ____|
+ | |__) | |__) | |  | | | | | |  | | | |   \ \_/ /| |__) | |__  | (___  
+ |  ___/|  _  /| |  | | | | | |  | | | |    \   / |  ___/|  __|  \___ \ 
+ | |    | | \ \| |__| | | | | |__| | | |     | |  | |    | |____ ____) |
+ |_|    |_|  \_\\____/  |_|  \____/  |_|     |_|  |_|    |______|_____/ */
+
+//main parsing function (calls others)
 string parse(string md_line, ifstream &fin, int &consecutive_blank_lines, bool &unclosed_p_tag);
-void parse_emphases_to_html(string &md_line);
+
+// === assorted parsing functions ===
+void parse_emphases_to_html_driver(string &md_line);
+void parse_emphases_to_html(string &md_line, const string &md_symbol,
+	const string &open_html_tag, const string &closed_html_tag);
 void parse_images_to_html(string &md_line);
 void parse_urls_to_html(string &md_line);
 void parse_inline_code_to_html(string &md_line);
 bool parse_blockquotes_to_html(string &md_line, bool inner_blockquote);
-void parse_list_to_html(const string &md_line, string &html_result);
+void parse_blockquotes_to_html(string &md_line, string &html_result, ifstream &fin);
+bool parse_headers_to_html(const string &md_line, string &html_result);
+bool parse_codeblock_to_html(string &md_line, string &html_result, ifstream &fin);
+bool parse_list_to_html(string &md_line, string &html_result, ifstream &fin);
 
-//helpers
+// === helpers ===
 void handle_errors(int argc, char **argv);
-void replace_emphases_helper(string &md_line, const string &md_symbol, 
-	const string &open_html_tag, const string &closed_html_tag);
+
 bool is_ulist_character(const string& c);
+bool is_olist_character(const string& c);
 int count_indents(const string& md_line);
 void append_tabs(string &html_result, int curr_list_level);
+
 string remove_leading_whitespace(const string &md_line);
-//thank you https://stackoverflow.com/a/14678800
+void close_open_p_tag(string &html_result, bool &unclosed_p_tag, int consecutive_blank_lines);
+
 string replace(std::string subject, const std::string& search, 
 	const std::string& replace);
-//thank you https://stackoverflow.com/a/14678800
 void replace_in_place(std::string &subject, const std::string& search, 
 	const std::string& replace);
 
+
+/*__  __          _____ _   _ 
+ |  \/  |   /\   |_   _| \ | |
+ | \  / |  /  \    | | |  \| |
+ | |\/| | / /\ \   | | | . ` |
+ | |  | |/ ____ \ _| |_| |\  |
+ |_|  |_/_/    \_\_____|_| \_|*/
 
 int main(int argc, char **argv)
 {
@@ -119,6 +148,14 @@ int main(int argc, char **argv)
 	fout << TAIL;
 }
 
+
+/*_____        _____   _____ _____ _   _  _____ 
+ |  __ \ /\   |  __ \ / ____|_   _| \ | |/ ____|
+ | |__) /  \  | |__) | (___   | | |  \| | |  __ 
+ |  ___/ /\ \ |  _  / \___ \  | | | . ` | | |_ |
+ | |  / ____ \| | \ \ ____) |_| |_| |\  | |__| |
+ |_| /_/    \_\_|  \_\_____/|_____|_| \_|\_____|*/
+
 string parse(string md_line, ifstream &fin, int &consecutive_blank_lines, bool &unclosed_p_tag)
 {
 	string html_result = "";
@@ -127,57 +164,24 @@ string parse(string md_line, ifstream &fin, int &consecutive_blank_lines, bool &
 	if(md_line == "---")
 	{
 		//<hr> tags can't be within <p> tags, so close any open <p>
-		if(unclosed_p_tag)
-		{
-			html_result = "</p>\n\n";
-			unclosed_p_tag = false;
-		}
-
-		consecutive_blank_lines = 0;
+		close_open_p_tag(html_result, unclosed_p_tag, consecutive_blank_lines);
 		return html_result + "<hr>";
 	}
 
 	//if the line only consists of <br> tags, don't wrap it in <p> tags
-	else if(md_line.length() >= 4 && md_line.substr(0,4) == "<br>" && 
+	if(md_line.length() >= 4 && md_line.substr(0,4) == "<br>" && 
 		replace(md_line, "<br>", "") == "")
 	{
 		consecutive_blank_lines = 0;
 		return md_line;
 	}
 
-	//code blocks
-	else if(md_line.substr(0,3) == "```")
+	bool is_codeblock = parse_codeblock_to_html(md_line, html_result, fin);
+	if(is_codeblock)
 	{
 		//<pre> tags can't be within <p> tags, so close any open <p>
-		if(unclosed_p_tag)
-		{
-			html_result += "</p>\n\n";
-			unclosed_p_tag = false;
-		}
-
-		consecutive_blank_lines = 0;
-
-		string language = md_line.substr(3);
-		html_result += "<pre><code class=\"" + language + "\">";
-		string line;
-		while(getline(fin, line))
-		{
-			if(line == "```") //closing backticks
-			{
-				html_result += "</code></pre>";
-				return html_result;
-			}
-			else
-			{
-				replace_in_place(line, "&", "&amp;");
-				replace_in_place(line, "<", "&lt;");
-				replace_in_place(line, ">", "&gt;");
-				html_result += line + "\n";
-			}
-		}
-
-		cout << "Invalid Markdown: Missing a closing code block! (```)\n";
-		exit(1);
+		close_open_p_tag(html_result, unclosed_p_tag, consecutive_blank_lines);
+		return html_result;
 	}
 
 	parse_inline_code_to_html(md_line);
@@ -186,106 +190,23 @@ string parse(string md_line, ifstream &fin, int &consecutive_blank_lines, bool &
 
 	parse_urls_to_html(md_line);
 
-	//bold, italics, and strikethrough
-	parse_emphases_to_html(md_line);
+	parse_emphases_to_html_driver(md_line); //bold, italics, and strikethrough
 
 	//headers, except where entire line is made up of #'s
-	//purposely placed at bottom cause they can contain markdown
-	if(md_line[0] == '#' && replace(md_line, "#", "") != "")
+	//purposely placed at bottom cause headers can contain markdown
+	bool is_header = parse_headers_to_html(md_line, html_result);
+	if(is_header)
 	{
 		//prefer header tags to be outside of <p> tags
-		if(unclosed_p_tag)
-		{
-			html_result = "</p>\n\n";
-			unclosed_p_tag = false;
-		}
-		consecutive_blank_lines = 0;
-
-		int hash_count = 0;
-		int i = 0;
-		char c = md_line[0];
-
-		while(c == '#' && hash_count < 6) //6 because of <h6>
-		{
-			++hash_count;
-			++i;
-			c = md_line[i];
-		}
-
-		html_result += "<h" + to_string(hash_count) + ">" +
-			md_line.substr(i) + "</h" + to_string(hash_count) + ">";
-
-		//return right away cause headers don't need to be parsed further
+		close_open_p_tag(html_result, unclosed_p_tag, consecutive_blank_lines);
 		return html_result;
 	}
 
-	else if(md_line.length() >= 2 && is_ulist_character(md_line.substr(0, 2)))
+	bool is_list = parse_list_to_html(md_line, html_result, fin);
+	if(is_list)
 	{
 		//lists aren't phrasing content, close p tag
-		if(unclosed_p_tag)
-		{
-			html_result = "</p>\n\n";
-			unclosed_p_tag = false;
-		}
-		consecutive_blank_lines = 0;
-
-		//begin list and append first bullet
-		html_result += "<ul>\n";
-		string content = md_line.substr(2); //everything after "* "
-		html_result += "\t<li>" + content + "</li>\n";
-
-		int curr_list_level = 0;
-		int indents = 0;
-
-		streampos prev_line = fin.tellg();
-
-		//loop until md_line is no longer part of the list (doesn't start with "* " )
-		while(getline(fin, md_line))
-		{
-			string trimmed_md_line = remove_leading_whitespace(md_line);
-			if(md_line.length() >= 2 && is_ulist_character(trimmed_md_line.substr(0, 2)))
-			{
-				//line is still part of the list
-
-				prev_line = fin.tellg();
-
-				//create/close nested list depending on num indents in the line
-				indents = count_indents(md_line);
-
-				if(indents > curr_list_level)
-				{
-					append_tabs(html_result, curr_list_level + 1); //for html code indentation
-					html_result += "<ul>\n";
-					curr_list_level++;
-				}
-				else if(indents < curr_list_level)
-				{
-					append_tabs(html_result, curr_list_level + 1); //for html code indentation
-					html_result += "</ul>\n";
-					curr_list_level--;
-				}
-
-				append_tabs(html_result, curr_list_level + 1); //for html code indentation
-
-				content = trimmed_md_line.substr(2); //cut off "* "
-				html_result += "<li>" + content + "</li>\n";
-			}
-			else
-			{
-				fin.seekg(prev_line); //rewind file to parse this line again, not as a list
-				break;
-			}
-		}
-
-		//close all open <ul> tags
-		for(int i = 0; i < curr_list_level + 1; ++i)
-		{
-			for(int j = curr_list_level - i; j > 0; --j) //for html code indentation
-				html_result += "\t";
-
-			html_result += "</ul>\n";
-		}
-
+		close_open_p_tag(html_result, unclosed_p_tag, consecutive_blank_lines);
 		return html_result;
 	}
 
@@ -294,44 +215,8 @@ string parse(string md_line, ifstream &fin, int &consecutive_blank_lines, bool &
 	if(is_blockquote)
 	{
 		//<blockquote> tags can't be within <p> tags, so close any open <p>
-		if(unclosed_p_tag)
-		{
-			html_result = "</p>\n\n";
-			unclosed_p_tag = false;
-		}
-		consecutive_blank_lines = 0;
-
-		html_result += md_line;
-
-		//consecutive >blockquotes should be in the same <blockquote> tag
-		//the following reads in any consecutive blockquotes into the tag
-
-		//save position of the line so we can rewind the file back once we read too far
-		streampos prev_line = fin.tellg();
-		while(getline(fin, md_line) && is_blockquote)
-		{
-			//blockquotes can contain markdown, so do the processing
-			parse_inline_code_to_html(md_line);
-			parse_images_to_html(md_line);
-			parse_urls_to_html(md_line);
-			parse_emphases_to_html(md_line);
-
-			is_blockquote = parse_blockquotes_to_html(md_line, true);
-
-			if(!is_blockquote)
-			{
-				//no more blockquotes, rewind file back 1 line so it can be parsed next time
- 				fin.seekg(prev_line);
-			}
-			else
-			{
-				//this is another blockquote, simply append content to open <blockquote> tag
-				prev_line = fin.tellg();
-				html_result += md_line;
-			}
-		}
-
-		html_result += "</blockquote>";
+		close_open_p_tag(html_result, unclosed_p_tag, consecutive_blank_lines);
+		parse_blockquotes_to_html(md_line, html_result, fin);
 		return html_result;
 	}
 
@@ -345,14 +230,12 @@ string parse(string md_line, ifstream &fin, int &consecutive_blank_lines, bool &
 			//this is achieved by either closing a <p> tag and starting a new one,
 			if(unclosed_p_tag)
 				html_result += "</p>\n\n<p>\n";
-
 			//or by simply opening a <p> tag if one isn't already open
 			else
 			{
 				html_result += "<p>\n";
 				unclosed_p_tag = true;
 			}
-
 			consecutive_blank_lines = 0;
 		}
 		else
@@ -367,7 +250,6 @@ string parse(string md_line, ifstream &fin, int &consecutive_blank_lines, bool &
 				unclosed_p_tag = true;
 			}
 		}
-
 		html_result += md_line;
 	}
 	else
@@ -377,15 +259,15 @@ string parse(string md_line, ifstream &fin, int &consecutive_blank_lines, bool &
 	return html_result;
 }
 
-void parse_emphases_to_html(string &md_line)
+void parse_emphases_to_html_driver(string &md_line)
 {
-	replace_emphases_helper(md_line, "**", "<b>", "</b>");
-	replace_emphases_helper(md_line, "~~", "<s>", "</s>");
-	replace_emphases_helper(md_line, "__", "<i>", "</i>");
+	parse_emphases_to_html(md_line, "**", "<b>", "</b>");
+	parse_emphases_to_html(md_line, "~~", "<s>", "</s>");
+	parse_emphases_to_html(md_line, "__", "<i>", "</i>");
 }
 
 //parses markdown emphasis syntax into html, in place
-void replace_emphases_helper(string &md_line, const string &md_symbol, 
+void parse_emphases_to_html(string &md_line, const string &md_symbol, 
 	const string &open_html_tag, const string &close_html_tag)
 {
 	const int symbol_len = 2; //this only works for symbols of length 2
@@ -630,6 +512,168 @@ void parse_inline_code_to_html(string &md_line)
 	}
 }
 
+bool parse_headers_to_html(const string &md_line, string &html_result)
+{
+	if(md_line[0] == '#' && replace(md_line, "#", "") != "")
+	{
+		int hash_count = 0;
+		int i = 0;
+		char c = md_line[0];
+
+		while(c == '#' && hash_count < 6) //6 because of <h6>
+		{
+			++hash_count;
+			++i;
+			c = md_line[i];
+		}
+
+		html_result += "<h" + to_string(hash_count) + ">" +
+			md_line.substr(i) + "</h" + to_string(hash_count) + ">";
+
+		return true;
+	}
+	else return false;
+}
+
+bool parse_codeblock_to_html(string &md_line, string &html_result, ifstream &fin)
+{
+	if(md_line.substr(0,3) == "```")
+	{
+		string language = md_line.substr(3);
+		html_result += "<pre><code class=\"" + language + "\">";
+
+		while(getline(fin, md_line))
+		{
+			if(md_line == "```") //closing backticks
+			{
+				html_result += "</code></pre>";
+				return true;
+			}
+			else
+			{
+				replace_in_place(md_line, "&", "&amp;");
+				replace_in_place(md_line, "<", "&lt;");
+				replace_in_place(md_line, ">", "&gt;");
+				html_result += md_line + "\n";
+			}
+		}
+
+		cout << "Missing a closing code block! (```)\n";
+		exit(1);
+	}
+
+	return false;
+}
+
+bool parse_list_to_html(string &md_line, string &html_result, ifstream &fin)
+{
+	if(md_line.length() < 2) //all list syntax has at least 2 chars
+		return false;
+
+	string open_tag = "", closed_tag = "";
+
+	if(is_ulist_character(md_line.substr(0, 2)))
+	{
+		open_tag = "<ul>";
+		closed_tag = "</ul>";
+	}
+	else if(is_olist_character(md_line.substr(0, 2)))
+	{
+		open_tag = "<ol>";
+		closed_tag = "</ol>";
+	}
+
+	if(open_tag != "")
+	{
+		//this is a list
+
+		//begin list and append first bullet
+		html_result += open_tag + "\n";
+		string content = md_line.substr(2); //everything after "* "
+		html_result += "\t<li>" + content + "</li>\n";
+
+		int curr_list_level = 0; //change to curr_nest_level
+		int indents = 0;
+
+		streampos prev_line = fin.tellg();
+
+		//loop until md_line is no longer part of the list (doesn't start with "* " )
+		while(getline(fin, md_line))
+		{
+			string trimmed_md_line = remove_leading_whitespace(md_line);
+			bool is_list = false;
+
+			if(md_line.length() >= 2)
+			{
+				//different list types can be nested, must recheck for each line
+
+				if(is_ulist_character(trimmed_md_line.substr(0, 2)))
+				{
+					open_tag = "<ul>";
+					closed_tag = "</ul>";
+					is_list = true;
+				}
+				else if(is_olist_character(trimmed_md_line.substr(0, 2)))
+				{
+					open_tag = "<ol>";
+					closed_tag = "</ol>";
+					is_list = true;
+				}
+			}
+
+			if(is_list)
+			{
+				//line is still part of the list
+
+				prev_line = fin.tellg();
+
+				//create/close nested list depending on num indents in the line
+				indents = count_indents(md_line);
+
+				if(indents > curr_list_level)
+				{
+					append_tabs(html_result, curr_list_level + 1); //for html code indentation
+					html_result += open_tag + "\n";
+					curr_list_level++;
+				}
+				else if(indents < curr_list_level)
+				{
+					append_tabs(html_result, curr_list_level + 1); //for html code indentation
+					html_result += closed_tag + "\n";
+					curr_list_level--;
+				}
+
+				append_tabs(html_result, curr_list_level + 1); //for html code indentation
+
+				content = trimmed_md_line.substr(2); //cut off "* "
+				parse_inline_code_to_html(content);
+				parse_images_to_html(content);
+				parse_urls_to_html(content);
+				parse_emphases_to_html_driver(content);
+				html_result += "<li>" + content + "</li>\n";
+			}
+			else
+			{
+				fin.seekg(prev_line); //rewind file to parse this line again, not as a list
+				break;
+			}
+		}
+
+		//close all open list tags
+		for(int i = 0; i < curr_list_level + 1; ++i)
+		{
+			for(int j = curr_list_level - i; j > 0; --j) //for html code indentation
+				html_result += "\t";
+
+			html_result += closed_tag + "\n";
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
 bool parse_blockquotes_to_html(string &md_line, bool inner_blockquote)
 {
 	if(md_line[0] == '>' && md_line.length() > 1)
@@ -644,20 +688,49 @@ bool parse_blockquotes_to_html(string &md_line, bool inner_blockquote)
 	else return false;
 }
 
-// void parse_list_to_html(const string &md_line, string &html_result)
-// {
-// 	html_result += "<ul>\n";
-// 	string content = md_line.substr(2); //everything after "* "
-// 	html_result += "\t<li>" + content = "</li>\n";
-// }
+void parse_blockquotes_to_html(string &md_line, string &html_result, ifstream &fin)
+{
+	html_result += md_line;
 
+	//consecutive >blockquotes should be in the same <blockquote> tag
+	//the following reads in any consecutive blockquotes into the tag
 
+	//save position of the line so we can rewind the file back once we read too far
+	streampos prev_line = fin.tellg();
 
-/* 
-==========
-HELPERS
-==========
-*/
+	bool is_blockquote = true;
+	while(getline(fin, md_line) && is_blockquote)
+	{
+		//blockquotes can contain markdown, so do the processing
+		parse_inline_code_to_html(md_line);
+		parse_images_to_html(md_line);
+		parse_urls_to_html(md_line);
+		parse_emphases_to_html_driver(md_line);
+
+		is_blockquote = parse_blockquotes_to_html(md_line, true);
+
+		if(!is_blockquote)
+		{
+			//no more blockquotes, rewind file back 1 line so it can be parsed next time
+				fin.seekg(prev_line);
+		}
+		else
+		{
+			//this is another blockquote, simply append content to open <blockquote> tag
+			prev_line = fin.tellg();
+			html_result += md_line;
+		}
+	}
+
+	html_result += "</blockquote>";
+}
+
+/*_    _ ______ _      _____  ______ _____   _____ 
+ | |  | |  ____| |    |  __ \|  ____|  __ \ / ____|
+ | |__| | |__  | |    | |__) | |__  | |__) | (___  
+ |  __  |  __| | |    |  ___/|  __| |  _  / \___ \ 
+ | |  | | |____| |____| |    | |____| | \ \ ____) |
+ |_|  |_|______|______|_|    |______|_|  \_\_____/ */
 
 //thank you https://stackoverflow.com/a/14678800
 string replace(std::string subject, const std::string& search, const std::string& replace)
@@ -691,7 +764,12 @@ void replace_in_place(std::string &subject, const std::string& search, const std
 
 bool is_ulist_character(const string& c)
 {
-	return c == "* " || c == "+ " || c == "- ";
+	return c[1] == ' ' && (c[0] == '*' || c[0] == '+' || c[0] == '-');
+}
+
+bool is_olist_character(const string& c)
+{
+	return c[1] == '.' && (isdigit(c[0]));
 }
 
 int count_indents(const string& md_line)
@@ -730,6 +808,17 @@ void append_tabs(string &html_result, int curr_list_level)
 	{
 		html_result += "\t";
 	}
+}
+
+void close_open_p_tag(string &html_result, bool &unclosed_p_tag, int consecutive_blank_lines)
+{
+	if(unclosed_p_tag)
+	{
+		html_result = "</p>\n\n" + html_result;
+		unclosed_p_tag = false;
+	}
+
+	consecutive_blank_lines = 0;
 }
 
 void handle_errors(int argc, char **argv)
